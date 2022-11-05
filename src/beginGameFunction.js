e39b1a1892d6d9a3c1f52c2cf93aa739c6c31895
Brooks Susor <susorbrooks@gmail.com>
2022-11-05 11:53:29 -0400

    Finished for now. Not super proud, but I want to move on and it has EXTREMELY basic functionality.

--- src/beginGameFunction.js
+++ src/beginGameFunction.js
@@ -1,65 +1,49 @@
 import { Human, Computer } from './objects';
 
 export const beginGame = () => {
-  const humanPlayer = new Human(prompt('What\'s your name?'));
+  const humanPlayer = new Human('Brooks');
   const computerPlayer = new Computer();
+  let humanTurn = true;
+  let computerTurn = false;
   let finished = false;
-  let currentAttack;
-
+  
   while (!finished) {
+    let currentAttack;
+
     // the human dishes out an attack
-    if (humanPlayer.yourTurn) {
-      currentAttack = humanPlayer.giveAttack(JSON.parse(prompt('Enter Attack Coordinates')));
-      //computerPlayer.playerBoard.arrShip.forEach(ship => {
-        //if (ship.coordinates.coord1 === currentAttack[0]
-        //&& ship.coordinates.coord2 === currentAttack[1]) {
-          console.log('You hit a ship!');
-        //}
-      //});
+    if (humanTurn) {
+      currentAttack = JSON.parse((prompt('Enter Attack Coordinates. Example: [1, 2]')));
 
       // the computer receives an attack
       computerPlayer.playerBoard.receiveAttack(currentAttack);
       
       // check if the human won
-      if (humanPlayer.playerBoard.allShipsSunk()) {
+      if (computerPlayer.playerBoard.allShipsSunk()) {
         finished = true;
         console.log('You\'ve sunk all their ships!');
-        return finished;
       }
       // now it's the computer's turn
-      humanPlayer.yourTurn = false;
-      computerPlayer.yourTurn = true;
+      humanTurn = false;
+      computerTurn = true;
+
+      console.log(humanPlayer);
       
       // the computer dishes out an attack
-    } else if (computerPlayer.yourTurn) {
+    } else if (computerTurn) {
       currentAttack = computerPlayer.randomAttack();
 
       // the human receives an attack
       humanPlayer.playerBoard.receiveAttack(currentAttack);
       
       // check if the computer won
-      if (computerPlayer.playerBoard.allShipsSunk()) {
+      if (humanPlayer.playerBoard.allShipsSunk()) {
         finished = true;
         console.log('All of your ships have been sunk!');
-        return finished;
       }
 
       // now it's the human's turn
-      computerPlayer.yourTurn = false;
-      humanPlayer.yourTurn = true;
+      computerTurn = false;
+      humanTurn = true;
     }
   }
 };
-
-/* // check for a winner, end the game if a player has won
-    if (humanPlayer.playerBoard.allShipsSunk()
-    || computerPlayer.playerBoard.allShipsSunk()) {
-      finished = true;
-      if (computerPlayer.playerBoard.allShipsSunk()) {
-        console.log('You\'ve sunk all their ships!');
-        return null;
-      } else if (humanPlayer.playerBoard.allShipsSunk()) {
-        console.log('All your ships have been sunk!');
-        return null;
-      }
-    } */
--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,41 @@
 export class Ship {
-  constructor(shipLength) {
+  constructor(shipLength, shipName) {
+    this.shipName = shipName;
     this.shipLength = shipLength;
     this.hits = 0;
-    this.sunk = false;
     this.coordinates = {
-      coord1: [1, 3],
-      coord2: [1, 9]
+      coord1: [0, 1],
+      coord2: [0, 5]
     };
   }
 
   hit() {
-    return this.hits += 1;
+    console.log('It\'s a hit!')
+    this.hits += 1;
   }
 
   isSunk() {
-    if (this.shipLength === this.hits) {
-      this.sunk = true;
-      return this.sunk;
+    if (this.shipLength <= this.hits) {
+      return true;
     }
   }
 }
 
 export class Gameboard {
   constructor() {
-    this.carrier = new Ship(6);
-    this.battleship = new Ship(4);
-    this.cruiser = new Ship(3);
-    this.submarine = new Ship(3);
-    this.destroyer = new Ship(2);
-    this.testBoat = new Ship(6);
+    this.carrier = new Ship(6, 'carrier');
+    this.battleship = new Ship(4, 'battleship');
+    this.cruiser = new Ship(3, 'cruiser');
+    this.submarine = new Ship(3, 'submarine');
+    this.destroyer = new Ship(2, 'destroyer');
+    //this.testBoat = new Ship(6, 'testBoat');
     this.arrShip = [
       this.carrier,
       this.battleship,
       this.cruiser,
       this.submarine,
       this.destroyer,
-      this.testBoat
+      //this.testBoat
     ];
     this.misses = [];
     this.sunkCount = 0;
@@ -55,37 +55,30 @@ export class Gameboard {
       const shipCoords1 = ship.coordinates.coord1;
       const shipCoords2 = ship.coordinates.coord2;
       
-      // Horizontal logic
+      // is the boat horizontal?
       if ((shipCoords1[0] === shipCoords2[0])
       && (coord[0] === shipCoords1[0])) {
+        // if so, is it within these coordinates?
         if ((coord[1] >= shipCoords1[1])
         && (coord[1] <= shipCoords2[1])) {
           ship.hit();
-          ship.isSunk();
-          if (ship.sunk) {
+          if (ship.isSunk()) {
             this.sunkCount += 1;
-            this.allShipsSunk();
           }
-          return null;
         } 
-      // Vertical logic
+      // is the boat vertical?
       } else if ((shipCoords1[1] === shipCoords2[1])
       && (coord[1] === shipCoords1[1])) {
+        // if so, is it within these coordinates?
         if ((coord[0] >= shipCoords1[0])
         && (coord[0] <= shipCoords2[0])) {
           ship.hit();
-          ship.isSunk();
-          if (ship.sunk) {
+          if (ship.isSunk()) {
             this.sunkCount += 1;
-            this.allShipsSunk();
           }
-          return null;
         }
       }
     });
-
-    // On a miss, push the coord to misses array
-    this.misses.push(coord);
   }
 }
 
@@ -99,18 +92,12 @@ export class Player {
 export class Human extends Player {
   constructor(playerName) {
     super(playerName);
-    this.yourTurn = true;
-  }
-
-  giveAttack(attack) {
-    return attack;
   }
 }
 
 export class Computer extends Player {
   constructor(playerName = 'Computer') {
     super(playerName);
-    this.yourTurn = false;
   }
 
   randomAttack() {
